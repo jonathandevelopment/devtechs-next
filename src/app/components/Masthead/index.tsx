66c7b5583836b9668a333644a229d04dc903d563
Jonathancodesurfer <jonathanalpizar43@gmail.com>
2024-02-28 11:24:28 -0600

    added

--- src/app/components/Masthead/index.tsx
+++ src/app/components/Masthead/index.tsx
@@ -115,7 +115,7 @@ export default function Masthead() {
                 <div className="mt-10 flex items-center gap-x-6">
                   <a
                     href="#"
-                    className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                    className="rounded-md bg-blue-700 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
                   >
                     Get it Touch
                   </a>
--- src/app/components/Services/index.tsx
+++ src/app/components/Services/index.tsx
@@ -9,7 +9,7 @@ import {
   
   const features = [
     {
-      name: 'Web development',
+      name: 'Web development:',
       description: 'Embark on a journey of digital innovation with our expert team proficient in full-stack development. We craft bespoke websites using cutting-edge frameworks to ensure seamless functionality and unparalleled user experiences.',
       icon: Cog6ToothIcon   ,
     },
@@ -42,25 +42,25 @@ import {
   
   export default function Services() {
     return (
-      <div className="bg-white py-24 sm:py-20">
+      <div className="bg-gray-900 py-24 sm:py-20">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <div className="mx-auto max-w-2xl sm:text-center">
-            <h2 className="text-base font-semibold leading-7 text-blue-800">Everything you need</h2>
-            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">No Website? No problem.</p>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+          <div className="max-w-2xl ">
+            <h2 className="text-base font-semibold leading-7 text-blue-700">Everything you need</h2>
+            <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">No Website? No problem.</p>
+            <p className="mt-6 text-lg leading-8 text-gray-100">
             Need an online presence but don't have a website? At <em>DevTechs</em>, we offer comprehensive web development and technology solutions to help you establish your online presence hassle-free. 
             </p>
           </div>
         </div>
         <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
-          <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
+          <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6 gap-y-10 text-base leading-7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
             {features.map((feature) => (
               <div key={feature.name} className="relative pl-9">
-                <dt className="inline font-semibold text-gray-900">
+                <dt className="inline font-bold text-white text-lg">
                   <feature.icon className="absolute left-1 top-1 h-5 w-5 text-blue-700" aria-hidden="true" />
                   {feature.name}
                 </dt>{' '}
-                <dd className="inline">{feature.description}</dd>
+                <dd className="inline text-lg">{feature.description}</dd>
               </div>
             ))}
           </dl>
